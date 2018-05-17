088809a5dce61fd21d92a992da94321c28bcac15
Fernando Borges <fernandoborgesjp@gmail.com>
2018-05-16 22:09:21 -0300

    inversão de controle - injeção de dependência e container

---
+++ spring/src/main/java/br/com/fernando/borges/Programa.java
@@ -0,0 +1,23 @@
+package br.com.fernando.borges;
+
+import br.com.fernando.borges.Pessoa;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class Programa {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(Programa.class);
+
+        Pessoa pessoa = context.getBean(Pessoa.class);
+
+        pessoa.setNome("Eduardo");
+        pessoa.setIdade(32);
+        System.out.println(pessoa);
+    }
+
+}
